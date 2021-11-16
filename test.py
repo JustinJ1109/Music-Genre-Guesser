fe253762bdd86620ac5065608437a1d5967517f4
justinj1109 <justin.jahlas11@gmail.com>
2021-11-16 16:37:05 -0500

    started on test.py

--- test.py
+++ test.py
@@ -1,3 +1,27 @@
-import tensorflow
+import imageio
+import pathlib
+import tensorflow as tf
+import keras
+from tensorflow._api.v2 import data
+import numpy as np
+import pandas as pd
 
-print("Hello World!")
+# image shape - (288, 432, 4)
+
+file_path = 'Data/images_original/'
+
+def load_data(data_path):
+    #https://stackoverflow.com/questions/60312574/how-to-read-multiple-directories-which-have-different-number-of-files-into-tenso
+    base_dir = pathlib.Path(data_path)
+    scenes = tf.data.Dataset.list_files(str(base_dir / '*'))
+
+    for s in scenes:
+        scene_dir = pathlib.Path(s.numpy().decode('utf-8'))
+        images = scene_dir / '*.png'
+        data = []
+        for i in images:
+            # features = [i['x1'], i['x2']] # ...
+            # data.append(features)
+            print(str(i))
+
+load_data(file_path)
