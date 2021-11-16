c54ba13fa80ca03645b432f4de58e55623339733
Lucas Seeterlin <seeterll@mail.gvsu.edu>
2021-11-16 16:40:29 -0500

    updated UI

--- GenreGuesser/src/app/app.component.ts
+++ GenreGuesser/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'GenreGuesser';
+  files: File[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  onSelect(event: any) {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+  }
+  
+  onRemove(event: any) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
+
+
 }
 
