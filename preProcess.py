3f7a112b192df0d84a796d17db1472d82e728dde
justin jahlas <justin.jahlas11@gmail.com>
2021-11-24 10:59:43 -0500

    added preprocessor for data -> mfcc

---
+++ preProcess.py
@@ -0,0 +1,59 @@
+import json
+import os
+import math
+import librosa
+
+DATASET_PATH = "Data/genres_original/"
+JSON_PATH = "preProcessedData/"
+SAMPLE_RATE = 22050
+TRACK_DURATION = 30 # measured in seconds
+SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
+
+
+def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+
+
+    samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+
+    # loop through all genre sub-folder
+    for (dirpath, dirnames, filenames) in os.walk(dataset_path):
+        data = []
+
+        # ensure we're processing a genre sub-folder level
+        if dirpath is not dataset_path:
+
+            # save genre label (i.e., sub-folder name) in the mapping
+            semantic_label = dirpath.split("/")[-1]
+            print("\nProcessing: {}".format(semantic_label))
+
+            # process all audio files in genre sub-dir
+            for f in filenames:
+
+		# load audio file
+                file_path = os.path.join(dirpath, f)
+                signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+
+                # process all segments of audio file
+                for d in range(num_segments):
+
+                    # calculate start and finish sample for current segment
+                    start = samples_per_segment * d
+                    finish = start + samples_per_segment
+
+                    # extract mfcc
+                    mfcc = librosa.feature.mfcc(signal[start:finish], sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
+                    mfcc = mfcc.T
+
+                    # store only mfcc feature with expected number of vectors
+                    if len(mfcc) == num_mfcc_vectors_per_segment:
+                        data.append(mfcc.tolist())
+                        print("{}, segment:{}".format(file_path, d+1))
+
+    # save MFCCs to json file
+            with open(json_path + semantic_label + ".json", "w") as fp:
+                json.dump(data, fp, indent=4)
+        
+        
+if __name__ == "__main__":
+    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)
