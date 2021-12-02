d90455bc8b6a419d895867aa2d46845dfa9815b4
Lucas Seeterlin <seeterll@mail.gvsu.edu>
2021-12-02 15:34:02 -0500

    connection

--- server.py
+++ server.py
@@ -3,51 +3,52 @@ from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
-
+from h5py._hl.files import File
+import test as t
 import os
 
 app = Flask(__name__)
 CORS(app)
 
-genre1 = "Country"
-genre2 = "Pop"
-genre3 = "Rock"
-percentage1 = 50
-percentage2 = 30
-percentage3 = 20
-
 UPLOAD_FOLDER = './uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-
-weather = {
-    "data": [
-    {
-        "genre1": genre1,
-        "percentage1": percentage1  
-    },
-    {
-        "genre2": genre2,
-        "percentage2": percentage2
-    },
-    {
-        "genre3": genre3,
-        "percentage3": percentage3
-    }
-    ]
-}
-
-
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    file: File = None
     if request.method == 'POST':
         file = request.files['file']
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
         return "File upload successful! Please go back and refresh."
 
     else :
-        return weather
+        print("reaching else")
+        if(os.listdir(UPLOAD_FOLDER)) != 0:
+            print(file)   
+            genre, percentage = t.predict(file)
+            print("ran python script")
+            predictions = {
+            "data": [
+            {
+                "genre1": genre[0],
+                "percentage1": percentage[0]  
+            },
+            {
+                "genre2": genre[1],
+                "percentage2": percentage[1] 
+            },
+            {
+                "genre3": genre[2],
+                "percentage3": percentage[2]
+            }
+            ]
+        }
+            return predictions
+        return ""
+       
+        
+
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -14,66 +14,72 @@ SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 
 MODEL_PATH = "saved_models/model-25612864e200b100ln3"
 
-model = load_model("saved_models/model-25612864e1000b100ln3t25.0")
+model = load_model("saved_models/model-25612864e200b100ln3")
 
 snippet_predictions = [0 for i in range(10)]
 
 samples_per_segment = int(SAMPLES_PER_TRACK / 10)
 num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / 512)
 
-#Trims track to 30 seconds.
-if (math.floor(librosa.get_duration(filename='test.wav')) > 30):
-    sampleRate, waveData = wavfile.read("test.wav")
-    startSample = 0
-    endSample = int(30 * sampleRate)
-    wavfile.write("test.wav", sampleRate, waveData[startSample:endSample])
+def predict(file):
+        #Trims track to 30 seconds.
+    if (math.floor(librosa.get_duration(filename=file)) > 30):
+        sampleRate, waveData = wavfile.read(file)
+        startSample = 0
+        endSample = int(30 * sampleRate)
+        wavfile.write(file, sampleRate, waveData[startSample:endSample])
 
-for i in range(10):
-    #Grabs a 3 second sample from the 30 second sample
-    sampleRate, waveData = wavfile.read("test.wav")
-    startSample = int(3 * i * sampleRate)
-    endSample = int(3 * (i+1) * sampleRate)
-    wavfile.write("snippet.wav", sampleRate, waveData[startSample:endSample])
+    for i in range(10):
+        #Grabs a 3 second sample from the 30 second sample
+        sampleRate, waveData = wavfile.read(file)
+        startSample = int(3 * i * sampleRate)
+        endSample = int(3 * (i+1) * sampleRate)
+        wavfile.write("snippet.wav", sampleRate, waveData[startSample:endSample])
+        
+        #Converts data to mfcc
+        signal, sample_rate = librosa.load("snippet.wav")
+        mfcc = librosa.feature.mfcc(signal, sample_rate, n_mfcc=13, n_fft=2048, hop_length=512)
+        mfcc = mfcc.T
+
+        prediction = tm.predict(model, mfcc)
+        snippet_predictions[np.argmax(prediction, axis=1)[0]] += 1
+
+    print(snippet_predictions)
+    topThree = [0 for i in range(3)]
+    topThreePercent = [0 for i in range(3)]
+
+    for i in range(3):
+        max_value = max(snippet_predictions)
+        topThreePercent[i] = max_value * 10
+        for j in range(10):
+            if (snippet_predictions[j] == max_value):
+                if (j == 0):
+                    topThree[i] = "Blues"
+                elif (j == 1):
+                    topThree[i] = "Classical"
+                elif (j == 2):
+                    topThree[i] = "Country"
+                elif (j == 3):
+                    topThree[i] = "Disco"
+                elif (j == 4):
+                    topThree[i] = "Hip Hop"
+                elif (j == 5):
+                    topThree[i] = "Jazz"
+                elif (j == 6):
+                    topThree[i] = "Metal"
+                elif (j == 7):
+                    topThree[i] = "Pop"
+                elif (j == 8):
+                    topThree[i] = "Reggae"
+                elif (j == 9):
+                    topThree[i] = "Rock"
+                snippet_predictions[j] = -1
+                break
+
+    print(topThree)
+    print(topThreePercent)
+
+    return topThree, topThreePercent
     
-    #Converts data to mfcc
-    signal, sample_rate = librosa.load("snippet.wav")
-    mfcc = librosa.feature.mfcc(signal, sample_rate, n_mfcc=13, n_fft=2048, hop_length=512)
-    mfcc = mfcc.T
 
-    prediction = tm.predict(model, mfcc)
-    snippet_predictions[np.argmax(prediction, axis=1)[0]] += 1
 
-print(snippet_predictions)
-topThree = [0 for i in range(3)]
-topThreePercent = [0 for i in range(3)]
-
-for i in range(3):
-    max_value = max(snippet_predictions)
-    topThreePercent[i] = max_value * 10
-    for j in range(10):
-        if (snippet_predictions[j] == max_value):
-            if (j == 0):
-                topThree[i] = "Blues"
-            elif (j == 1):
-                topThree[i] = "Classical"
-            elif (j == 2):
-                topThree[i] = "Country"
-            elif (j == 3):
-                topThree[i] = "Disco"
-            elif (j == 4):
-                topThree[i] = "Hip Hop"
-            elif (j == 5):
-                topThree[i] = "Jazz"
-            elif (j == 6):
-                topThree[i] = "Metal"
-            elif (j == 7):
-                topThree[i] = "Pop"
-            elif (j == 8):
-                topThree[i] = "Reggae"
-            elif (j == 9):
-                topThree[i] = "Rock"
-            snippet_predictions[j] = -1
-            break
-
-print(topThree)
-print(topThreePercent)
