c96a5e5092fe70d6cf7ed88803e3dd2d83a493f1
justin jahlas <justin.jahlas11@gmail.com>
2021-12-02 21:37:47 -0500

    finalized test.py

--- test.py
+++ test.py
@@ -12,8 +12,8 @@ SAMPLE_RATE = 22050
 TRACK_DURATION = 30 # measured in seconds
 SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 
-WAV_PATH = "test_music/get_off_my_dick.wav"
-MODEL_PATH = "saved_models/model-25612864e1000b100ln3t25.0model-51225664e200b32ln3t25.0"
+WAV_PATH = "test_music/pachelbel.wav"
+MODEL_PATH = "saved_models/model-512-256-64-e200b32ln3t25.0"
 
 model = load_model(MODEL_PATH)
 
